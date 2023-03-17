4b399bca5d6dff56e7647302ad1f265c1045d88c
Ana Delic <milojevican@gmail.com>
2023-03-17 15:35:02 +0100

    Delete type button in register form file

--- app/(auth)/signUp/RegisterForm.tsx
+++ app/(auth)/signUp/RegisterForm.tsx
@@ -65,10 +65,7 @@ export default function RegisterForm(props: { returnTo?: string | string[] }) {
         </label>
       </div>
       <div className="flex items-center justify-between">
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          type="button"
-        >
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
           Register
         </button>
       </div>
