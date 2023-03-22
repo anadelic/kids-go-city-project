0f1953fc195f07475ee4de1dd9ef353a8192f6f1
Ana Delic <milojevican@gmail.com>
2023-03-22 21:17:27 +0100

    Finish playwright tests

--- playwright/tests/navigate.spec.ts
+++ playwright/tests/navigate.spec.ts
@@ -1,10 +1,31 @@
-import { expect, test } from '@playwright/test';
+import { test } from '@playwright/test';
 
-test('navigation test', async ({ page }) => {
+test('navigation', async ({ page }) => {
   await page.goto('http://localhost:3000/');
-  await expect(page).toHaveURL('http://localhost:3000/');
-  await expect(
-    page.getByRole('heading', { name: 'Vienna with little ones' }),
-  ).toBeVisible();
-  await expect(page.locator('h1')).toHaveText('Vienna with little ones');
+  await page
+    .locator('div')
+    .filter({ hasText: '+− Leaflet | ©' })
+    .nth(2)
+    .click();
+
+  await page.getByRole('link', { name: 'Crafts' }).click();
+  await page.goto('http://localhost:3000/crafts');
+  await page.getByRole('link', { name: 'Weather' }).click();
+  await page.getByRole('button', { name: 'Show more outdoor places' }).click();
+  await page
+    .getByRole('link', {
+      name: 'Landgut Wien Cobenzl Am Cobenzl 96a, 1190 Wien',
+    })
+    .click();
+  await page.getByRole('link', { name: 'Home' }).click();
+});
+
+test('login', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.getByRole('link', { name: 'Login' }).click();
+  await page.getByPlaceholder('Please enter your username').click();
+  await page.getByPlaceholder('Please enter your username').fill('Ana');
+  await page.getByPlaceholder('Please enter your password').click();
+  await page.getByPlaceholder('Please enter your password').fill('12345');
+  await page.getByRole('button', { name: 'Login' }).click();
 });
